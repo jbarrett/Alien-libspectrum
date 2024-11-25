fb8d7ccf07d924e310b21132b7cfad2466f9a5d9
John Barrett <john@jbrt.org>
2024-11-25 12:04:52 +0000

    Basic working libsectrum Alien

--- lib/Alien/libspectrum.pm
+++ lib/Alien/libspectrum.pm
@@ -2,4 +2,12 @@ use strict;
 use warnings;
 package Alien::libspectrum;
 
+# ABSTRACT: Find or install libspectrum
+
+use parent qw/ Alien::Base /;
+
+# ALIEN SYNOPSIS
+# ALIEN DESCRIPTION
+# ALIEN SEE ALSO
+
 1;
